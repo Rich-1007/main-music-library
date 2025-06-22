8807ebf30ba3ce8071870e2b1602e3d139272397
Hricheak <ghricheak@gmail.com>
2025-06-22 11:57:27 +0530

    plugin federation : updates with live link

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,25 @@
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import federation from "@originjs/vite-plugin-federation";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
     federation({
       remotes: {
-        remote_app: "http://localhost:4173/assets/remoteEntry.js",
+        remote_app:
+          "https://music-library-hkpd3z2f0-manojs-projects-590e35f7.vercel.app/assets/remoteEntry.js",
       },
       shared: ["react", "react-dom"],
     }),
   ],
+  build: {
+    target: "esnext",
+    modulePreload: {
+      polyfill: false,
+    },
+  },
 });
